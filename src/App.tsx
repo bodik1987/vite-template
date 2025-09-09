c410b9ffaadef90a1dd54ecad693f1a9e697f5be
bogdan shulika <bsshul@gmail.com>
2025-09-09 10:11:17 +0200

    Delete heroui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { HeroUIProvider } from "@heroui/react";
 import Content from "./components/content/content";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header";
 
 export default function App() {
   return (
-    <HeroUIProvider>
-      <main className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
-        <Header />
-        <Content />
-        <Footer />
-      </main>
-    </HeroUIProvider>
+    <main className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
+      <Header />
+      <Content />
+      <Footer />
+    </main>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,49 +1,55 @@
-import { Button } from "@heroui/react";
+import { useState } from "react";
+import { motion } from "framer-motion";
 import { useUIStore } from "../../store/useStore";
-import { Tabs, Tab } from "@heroui/tabs";
+
+const tabs = [
+  { id: "element_1", label: "Кнопка 1" },
+  { id: "element_2", label: "Кнопка 2" },
+];
 
 export default function Footer() {
   const { setOpenBottomSheet, setContentKey } = useUIStore();
 
+  const [activeTab, setActiveTab] = useState(tabs[0].id);
+
   return (
     <footer className="select-none">
       <div className="wrapper pt-3 pb-8">
-        <div className="w-full flex items-center gap-3">
-          {/* Панель с кнопками
-          <div className="flex gap-3 w-full bg-gray-200 rounded-full p-[2px]">
-            <button
-              type="button"
-              className="rounded-button w-full text-gray-600"
-            >
-              Кнопка 1
-            </button>
-            <button
-              type="button"
-              className="rounded-button w-full bg-white border border-[#D7D7D8] shadow"
-            >
-              Кнопка 2
-            </button>
-          </div> */}
-
-          <div className="flex w-full flex-col">
-            <Tabs radius="full" fullWidth aria-label="Options" size="lg">
-              <Tab key="1" title="Кнопка 1" className="h-11"></Tab>
-              <Tab key="2" title="Кнопка 2" className="h-11"></Tab>
-            </Tabs>
+        <div className="flex items-center gap-3">
+          {/* Панель с кнопками */}
+          <div className="w-full mx-auto flex space-x-1 bg-gray-200 rounded-full p-1">
+            {tabs.map((tab) => (
+              <button
+                key={tab.id}
+                onClick={() => setActiveTab(tab.id)}
+                className={`relative rounded-full w-full rounded-button px-4 py-2 ${
+                  activeTab === tab.id
+                    ? "text-black" // активная кнопка → текст черный
+                    : "text-gray-700 hover:text-black/60"
+                }`}
+              >
+                {activeTab === tab.id && (
+                  <motion.span
+                    layoutId="bubble"
+                    className="absolute inset-0 z-10 bg-white rounded-full shadow"
+                    transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                  />
+                )}
+                <span className="relative z-20">{tab.label}</span>
+              </button>
+            ))}
           </div>
-          <Button
-            onPress={() => {
+
+          <button
+            onClick={() => {
               setOpenBottomSheet(true);
               setContentKey("list");
             }}
             type="button"
-            radius="full"
-            variant="light"
-            isIconOnly
-            className="w-13 h-11 aspect-square bg-accent text-white"
+            className="rounded-button w-12 aspect-square bg-accent text-white"
           >
             +
-          </Button>
+          </button>
         </div>
       </div>
     </footer>
