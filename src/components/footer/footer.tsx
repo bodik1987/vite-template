b6674f8492eb64063bfd33a73e3796704bc5fd91
bogdan shulika <bsshul@gmail.com>
2025-09-09 10:25:28 +0200

    Fix style

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
       <div className="wrapper pt-3 pb-8">
         <div className="flex items-center gap-3">
           {/* Панель с кнопками */}
-          <div className="w-full mx-auto flex space-x-1 bg-gray-200 border-y-2 border-t-gray-300 border-b-gray-50 rounded-full p-[3px] px-[4px]">
+          <div className="w-full mx-auto flex space-x-1 bg-gray-200 border-y-2 border-t-gray-300 border-b-gray-50 rounded-full p-[3px] pl-[4px] pr-[5px]">
             {tabs.map((tab) => (
               <button
                 key={tab.id}
@@ -32,7 +32,7 @@ export default function Footer() {
                   <motion.span
                     layoutId="bubble"
                     className="absolute inset-0 z-10 bg-gray-50 border-y border-t-white border-b-gray-300  rounded-full shadow"
-                    transition={{ type: "spring", bounce: 0.1, duration: 0.3 }}
+                    transition={{ type: "spring", bounce: 0.1, duration: 0.2 }}
                   />
                 )}
                 <span
